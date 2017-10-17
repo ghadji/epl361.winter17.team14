04a2bc950bd799e38cb7c677ede9ba800d292686
antreach <31773152+antreach@users.noreply.github.com>
2017-10-17 12:42:37 +0300

    Add files via upload
    Added ui elements inside settings pages

--- Mobile_App/js/index.js
+++ Mobile_App/js/index.js
@@ -1,13 +1,53 @@
 window.fn = {};
 
-window.fn.open = function() {
-  var menu = document.getElementById('menu');
-  menu.open();
+window.sources = {
+    sites: [],
+    total: 0,
+    canAddNew: true
+}
+
+window.fn.open = function () {
+    hideSubmenus();
+    var menu = document.getElementById('menu');
+    menu.open();
 };
 
-window.fn.load = function(page) {
-  var content = document.getElementById('content');
-  var menu = document.getElementById('menu');
-  content.load(page)
-    .then(menu.close.bind(menu));
+window.fn.load = function (page) {
+    var content = document.getElementById('content');
+    var menu = document.getElementById('menu');
+    content.load(page)
+        .then(menu.close.bind(menu));
+};
+
+window.fn.toggleSettings = function () {
+    var submenu = document.getElementById('settingsSubmenu');
+    if (submenu.style.display == 'none')
+        submenu.style.display = 'block';
+    else
+        submenu.style.display = 'none';
+}
+
+function hideSubmenus() {
+    var submenu = document.getElementById('settingsSubmenu');
+    submenu.style.display = 'none';
+
+}
+
+window.fn.showPopover = function (target) {
+    document.getElementById('fontSizePopover').show(target);
+};
+
+window.fn.hidePopover = function () {
+    document.getElementById('fontSizePopover').hide();
+};
+
+window.fn.nightMode = function () {
+    var value = document.getElementById('NightModeToggle').checked;
+    var el = document.getElementsByTagName('ons-page');
+    for (var i = 0; i < el.length; i++) {
+        if (value)
+            ons.modifier.add(el[i], 'night_mode_bg');
+        else
+            ons.modifier.remove(el[i], 'night_mode_bg');
+    }
 };
