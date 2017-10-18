16b47f86e9f17933e5489577c616278430c29b50
ghadji <ghadji12@cs.ucy.ac.cy>
2017-10-18 13:29:48 +0300

    Added filtering UI elements and basic functionality.

--- Mobile_App/js/index.js
+++ Mobile_App/js/index.js
@@ -9,6 +9,7 @@ window.sources = {
 window.fn.open = function () {
     hideSubmenus();
     var menu = document.getElementById('menu');
+    window.fn.repeatSources();
     menu.open();
 };
 
@@ -30,8 +31,75 @@ window.fn.toggleSettings = function () {
 function hideSubmenus() {
     var submenu = document.getElementById('settingsSubmenu');
     submenu.style.display = 'none';
+    var newSourceDiv = document.getElementById('newSourceDiv');
+    newSourceDiv.style.display = 'none';
+    var newSourceInput = document.getElementById('newSourceInput');
+    newSourceInput.value = '';
 
 }
+
+window.fn.repeatSources = function () {
+    var sourcesList = document.getElementById('sourcesList');
+
+    sourcesList.delegate = {
+        createItemContent: function (i) {
+            return ons._util.createElement(
+                '<ons-list-item class=\'menuStyle\'>' +
+                '<ons-button onclick="fn.removeSource(' + i + ')">' +
+                '<ons-icon icon="fa-times" tappable></ons-icon></ons-button>' +
+                window.sources.sites[i] + '</ons-list-item>'
+            );
+        },
+        countItems: function () {
+            return window.sources.total;
+        }
+    };
+
+    sourcesList.refresh();
+}
+
+window.fn.addSource = function () {
+    var newSourceDiv = document.getElementById('newSourceDiv');
+    if (window.sources.canAddNew) {
+        newSourceDiv.style.display = 'block';
+        window.sources.canAddNew = false;
+    } else {
+        newSourceDiv.style.display = 'none';
+        window.sources.canAddNew = true;
+    }
+}
+
+window.fn.addSourceToList = function () {
+    var newSourceInput = document.getElementById('newSourceInput');
+    if (newSourceInput.value.length > 0) {
+        window.sources.sites.push(newSourceInput.value);
+        window.sources.total++;
+        window.sources.canAddNew = true;
+        newSourceInput.value = '';
+        window.fn.repeatSources();
+    }
+}
+
+window.fn.removeSource = function (index) {
+    window.fn.confirmSourceDelete(index);
+}
+
+window.fn.confirmSourceDelete = function (material, index) {
+    ons.notification.confirm({
+        message: 'Are you sure you want to delete this source?',
+        callback: function (idx) {
+            switch (idx) {
+                case 0:
+                    break;
+                case 1:
+                    window.sources.total--;
+                    window.sources.sites.pop(index);
+                    window.fn.repeatSources();
+                    break;
+            }
+        }
+    });
+};
 
 window.fn.showPopover = function (target) {
     document.getElementById('fontSizePopover').show(target);
@@ -41,13 +109,10 @@ window.fn.hidePopover = function () {
     document.getElementById('fontSizePopover').hide();
 };
 
-window.fn.nightMode = function () {
-    var value = document.getElementById('NightModeToggle').checked;
-    var el = document.getElementsByTagName('ons-page');
-    for (var i = 0; i < el.length; i++) {
-        if (value)
-            ons.modifier.add(el[i], 'night_mode_bg');
-        else
-            ons.modifier.remove(el[i], 'night_mode_bg');
-    }
+window.fn.nightMode = function (check) {
+	var el = document.getElementById('app_page');
+	if (check.checked)
+		ons.modifier.add(el, 'night_mode_bg');
+	else
+		ons.modifier.remove(el, 'night_mode_bg');
 };
