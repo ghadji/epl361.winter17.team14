3c7b8ea7a059ac5e5337cd813b69f19889e3e8dd
ghadji <ghadji12@cs.ucy.ac.cy>
2017-10-20 18:39:34 +0300

    Added event listeners and welcome page

--- Mobile_App/js/index.js
+++ Mobile_App/js/index.js
@@ -1,18 +1,9 @@
 /*array that will contain all methods that will be used from HTML side*/
 window.fn = {};
 
-/*array containing the information about the sources array that will display the available sources*/
-window.sources = {
-    sites: [],
-    total: 0,
-    canAddNew: true
-}
-
 /*method responsible for opening the sidemenu*/
 window.fn.open = function () {
-    hideSubmenus();
     var menu = document.getElementById('menu');
-    window.fn.repeatSources();
     menu.open();
 };
 
@@ -33,100 +24,94 @@ window.fn.toggleSettings = function () {
         submenu.style.display = 'none';
 }
 
-/*method responsible for hiding all submenus that are available in the app*/
-function hideSubmenus() {
-    var submenu = document.getElementById('settingsSubmenu');
-    submenu.style.display = 'none';
-    var newSourceDiv = document.getElementById('newSourceDiv');
-    newSourceDiv.style.display = 'none';
-    var newSourceInput = document.getElementById('newSourceInput');
-    newSourceInput.value = '';
-
-}
-
-/*method responsible for repeating all available sources as a list for the user to choose from*/
-window.fn.repeatSources = function () {
-    var sourcesList = document.getElementById('sourcesList');
-
-    sourcesList.delegate = {
-        createItemContent: function (i) {
-            return ons._util.createElement(
-                '<ons-list-item class=\'menuStyle\'>' +
-                '<ons-button onclick="fn.removeSource(' + i + ')">' +
-                '<ons-icon icon="fa-times" tappable></ons-icon></ons-button>' +
-                window.sources.sites[i] + '</ons-list-item>'
-            );
-        },
-        countItems: function () {
-            return window.sources.total;
-        }
-    };
-
-    sourcesList.refresh();
-}
-
-/*method responsible for displaying an input for the user to type in the new source that they want to add*/
-window.fn.addSource = function () {
-    var newSourceDiv = document.getElementById('newSourceDiv');
-    if (window.sources.canAddNew) {
-        newSourceDiv.style.display = 'block';
-        window.sources.canAddNew = false;
-    } else {
-        newSourceDiv.style.display = 'none';
-        window.sources.canAddNew = true;
-    }
-}
-
-/*method responsible for adding the source typed by the user into the list of the user's selected sources*/
-window.fn.addSourceToList = function () {
-    var newSourceInput = document.getElementById('newSourceInput');
-    if (newSourceInput.value.length > 0) {
-        window.sources.sites.push(newSourceInput.value);
-        window.sources.total++;
-        window.sources.canAddNew = true;
-        newSourceInput.value = '';
-        window.fn.repeatSources();
-    }
-}
-
-/*method prompting for a confirmation if the user wants to delete the selected source*/
-window.fn.removeSource = function (index) {
-    window.fn.confirmSourceDelete(index);
-}
-
-/*method responsible for displaying said deletion prompt and then deleting the source if the user confirmed the prompt*/
-window.fn.confirmSourceDelete = function (material, index) {
-    ons.notification.confirm({
-        message: 'Are you sure you want to delete this source?',
-        callback: function (idx) {
-            switch (idx) {
-                case 0:
-                    break;
-                case 1:
-                    window.sources.total--;
-                    window.sources.sites.pop(index);
-                    window.fn.repeatSources();
-                    break;
-            }
-        }
-    });
-};
-
-/*method responsible for displaying a popover on font size change button*/
-window.fn.showPopover = function (target) {
-    document.getElementById('fontSizePopover').show(target);
-};
-
-/*method responsible for hiding the popover on font size change button*/
-window.fn.hidePopover = function () {
-    document.getElementById('fontSizePopover').hide();
-};
-
 /*method responsible for toggling the night mode modifier on and off the elements*/
-window.fn.nightMode = function (check) {
-    var el = document.getElementById('app_page');
-    if (check.checked)
-        ons.modifier.add(el, 'night_mode_bg');
-    else
-        ons.modifier.remove(el, 'night_mode_bg');
+window.fn.nightMode = function (el) {
+    var value = el.checked;
+    window.localStorage.setItem('isNightMode', value);
+    setNightMode();
 };
+
+/* toggles cache news setting*/
+window.fn.cacheNews = function (el) {
+    var value = el.checked;
+    window.localStorage.setItem('isCachingNews', value);
+}
+
+/*toggles markup toxic content setting*/
+window.fn.markupToxic = function (el) {
+    var value = el.checked;
+    window.localStorage.setItem('isMarkupToxic', value);
+}
+
+/*toggles hide toxic content setting*/
+window.fn.hideToxic = function (el) {
+    var value = el.checked;
+    window.localStorage.setItem('isHideToxic', value);
+}
+
+/*goes from welcome page to home page*/
+window.fn.goToHome = function () {
+    var nav = document.getElementById('welcomeScreenNavigator');
+    nav.pushPage('home.html', { animation: 'slide' });
+}
+
+/*enables/disables night mode*/
+function setNightMode() {
+    var value = JSON.parse(window.localStorage.getItem('isNightMode'));
+    var pages = document.getElementsByTagName('ons-page');
+    var list = document.getElementById('sidemenuList');
+    var paragraphs = document.getElementsByTagName('TextTag');
+    for (var i = 0; i < pages.length; i++) {
+        if (value)
+            ons.modifier.add(pages[i], 'night_mode_bg');
+        else
+            ons.modifier.remove(pages[i], 'night_mode_bg');
+    }
+    for (var i = 0; i < paragraphs.length; i++) {
+        if (value)
+            paragraphs[i].classList.add('night_mode_text');
+            //ons.modifier.add(paragraphs[i], 'night_mode_text');
+        else
+            paragraphs[i].classList.remove('night_mode_text');
+        //ons.modifier.remove(paragraphs[i], 'night_mode_text');
+    }
+    if (value)
+        ons.modifier.add(list, 'night_mode_bg');
+    else 
+
+        ons.modifier.remove(list, 'night_mode_bg');
+};
+
+/*EVENT LISTENERS*/
+document.addEventListener('init', function (event) {
+    var page = event.target;
+    if (page.matches('#displaySettingsPage')) {
+        document.getElementById('NightModeToggle').checked =
+                            JSON.parse(window.localStorage.getItem('isNightMode'));
+        document.getElementById('CacheNewsToggle').checked =
+                            JSON.parse(window.localStorage.getItem('isCachingNews'));
+    } else if (page.matches('#sideMenu')) {
+        document.getElementById('settingsSubmenu').style.display = 'none';
+    } else if (page.matches('#filteringSettingsPage')) {
+        document.getElementById('MarkupToxicToggle').checked =
+                            JSON.parse(window.localStorage.getItem('isMarkupToxic'));
+        document.getElementById('HideToxicToggle').checked =
+                            JSON.parse(window.localStorage.getItem('isHideToxic'));
+        document.getElementById('ToxicToleranceSlider').value =
+                            JSON.parse(window.localStorage.getItem('toxicToleranceValue'));
+    }
+    setNightMode();
+});
+
+document.addEventListener('postclose', function (event) {
+    var page = event.target;
+    if (page.matches('#menu'))
+        document.getElementById('settingsSubmenu').style.display = 'none';
+});
+
+document.addEventListener('hide', function (event) {
+    var page = event.target;
+    if (page.matches('#filteringSettingsPage'))
+        window.localStorage.setItem('toxicToleranceValue', document.getElementById('ToxicToleranceSlider').value);
+})
+/*EVENT LISTENERS*/
