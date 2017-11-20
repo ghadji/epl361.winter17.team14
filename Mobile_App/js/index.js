cc50ac91faf4fc015522f47cb1367696195f1909
Marios Kelepeshis <mkelep01@ucy.ac.cy>
2017-11-21 00:28:15 +0200

    Bug fixes
    - Finalized log in screen to store an array of accounts
    - Bug fixes on appearance
    - Commenting and beautification

--- Mobile_App/js/index.js
+++ Mobile_App/js/index.js
@@ -1,171 +1,217 @@
-ons.platform.select('android');
+ons.platform.select("android");
 
 /*array that will contain all methods that will be used from HTML side*/
 window.fn = {};
 
 window.data = {};
 
-function init(){
+function init() {
     window.fn.requestArticle();
     window.fn.getSources();
-};
+}
 
-window.fn.login = function(){
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-  
-    if (username === window.localStorage.getItem('username') && password === window.localStorage.getItem('password')) {
-        window.fn.goToMainView();
-        window.fn.hideModal();
-        ons.notification.toast({message: 'Welcome back ' + username + ' !', timeout: 3000});
-    }else {
-      ons.notification.alert('Incorrect username or password. Please try again.');
+/* function responsible to handle login*/
+window.fn.login = function() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var accountArr = JSON.parse(window.localStorage.getItem("account_array"));
+    var found = false;
+    if (username && password && accountArr && accountArr.length > 0) {
+        for (var i = 0; i < accountArr.length; i++) {
+            if (accountArr[i]["UserName"] == username && accountArr[i]["Password"] == password) {
+                found = true;
+                window.fn.goToMainView();
+                window.fn.hideModal();
+                ons.notification.toast({ message: "Welcome back " + username + " !", timeout: 3000 });
+            }
+        }
     }
+    if (!found)
+        ons.notification.alert("Incorrect username or password. Please try again.");
 };
 
-window.fn.register = function(){
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    if (username && password){
-        window.localStorage.setItem('username', username);
-        window.localStorage.setItem('password', password);
-        window.fn.hideModal();
-        window.fn.goToMainView();
-        ons.notification.toast({message: 'Welcome ' + username + ' !', timeout: 3000});
-    }else {
-      ons.notification.alert('Invalid username or password. Please try again.');
+/* function responsible to handle register*/
+window.fn.register = function() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var found = false;
+    var accountArr = JSON.parse(window.localStorage.getItem("account_array"))
+    console.log(accountArr);
+    if (!username && !password) {
+        ons.notification.alert("Invalid username or password. Please try again.");
     }
-    
+    if (accountArr != null && accountArr.length > 0) {
+        for (var i = 0; i < accountArr.length; i++) {
+            if (accountArr[i]["UserName"] == username) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            ons.notification.alert("Username is taken. Please try again.");
+            return;
+        }
+    } else if (accountArr == null) {
+        window.localStorage.removeItem("isNightMode");
+        window.localStorage.removeItem("isCachingNews");
+        window.localStorage.removeItem("isMarkupToxic");
+        window.localStorage.removeItem("isHideToxic");
+        window.localStorage.removeItem("toxicToleranceValue");
+        window.localStorage.removeItem("checkedSources");
+        accountArr = [];
+    }
+    accountArr.push({ UserName: username, Password: password });
+    window.localStorage.setItem("account_array", JSON.stringify(accountArr));
+    window.fn.hideModal();
+    window.fn.goToMainView();
+    ons.notification.toast({
+        message: "Welcome " + username + " !",
+        timeout: 3000
+    });
 };
 
-/*method responsible for opening the sidemenu*/
-window.fn.open = function () {
-    var menu = document.getElementById('menu');
+/*sponsible for opening the sidemenu*/
+window.fn.open = function() {
+    var menu = document.getElementById("menu");
     menu.open();
 };
 
 /*method responsible from loading the given page to viewport*/
-window.fn.load = function (page) {
-    var content = document.getElementById('content');
-    var menu = document.getElementById('menu');
-    content.load(page)
-        .then(menu.close.bind(menu));
+window.fn.load = function(page) {
+    var content = document.getElementById("content");
+    var menu = document.getElementById("menu");
+    content.load(page).then(menu.close.bind(menu));
 };
 
 /* toggles cache news setting*/
-window.fn.cacheNews = function (el) {
+window.fn.cacheNews = function(el) {
     var value = el.checked;
-    window.localStorage.setItem('isCachingNews', value);
-}
+    window.localStorage.setItem("isCachingNews", value);
+};
 
 /*toggles markup toxic content setting*/
-window.fn.markupToxic = function (el) {
+window.fn.markupToxic = function(el) {
     var value = el.checked;
-    window.localStorage.setItem('isMarkupToxic', value);
-}
+    window.localStorage.setItem("isMarkupToxic", value);
+};
 
 /*toggles hide toxic content setting*/
-window.fn.hideToxic = function (el) {
+window.fn.hideToxic = function(el) {
     var value = el.checked;
-    window.localStorage.setItem('isHideToxic', value);
-}
+    window.localStorage.setItem("isHideToxic", value);
+};
 
 /*goes from welcome page to home page*/
-window.fn.goToMainView = function () {
+window.fn.goToMainView = function() {
     //var main_page = document.getElementById('main_page');
     //nav.pushPage('home.html', { animation: 'slide' });
     //window.location = './index.html';
-    document.querySelector('#myNavigator').pushPage('mainView.html', {data: {title: 'Home'}});
-}
+    document
+        .querySelector("#myNavigator")
+        .pushPage("mainView.html", { data: { title: "Home" } });
+};
 
 window.fn.showModal = function() {
-    var modal = document.querySelector('ons-modal');
+    var modal = document.querySelector("ons-modal");
     modal.show();
-}
+};
 
-window.fn.hideModal = function(){
-    var modal = document.querySelector('ons-modal');
+window.fn.hideModal = function() {
+    var modal = document.querySelector("ons-modal");
     modal.hide();
-}
+};
 
-window.fn.changeFontSize = function (index) {
+window.fn.changeFontSize = function(index) {
     var el = document.getElementsByTagName("fontChangeTag");
     for (var i = 0; i < el.length; i++) {
         switch (index) {
-            case 0: el[i].style.fontSize = '100%';
+            case 0:
+                el[i].style.fontSize = "100%";
                 break;
-            case 1: el[i].style.fontSize = '110%';
+            case 1:
+                el[i].style.fontSize = "110%";
                 break;
-            case 2: el[i].style.fontSize = '120%';
+            case 2:
+                el[i].style.fontSize = "120%";
                 break;
-            case 3: el[i].style.fontSize = '130%';
+            case 3:
+                el[i].style.fontSize = "130%";
                 break;
         }
     }
-	if(el = document.getElementById('fontSizeText')) el.innerHTML = (index*2 + 20) + 'px';
-    window.localStorage.setItem('fontSizeIndex', index);
-}
+    if ((el = document.getElementById("fontSizeText")))
+        el.innerHTML = index * 2 + 20 + "px";
+    window.localStorage.setItem("fontSizeIndex", index);
+};
 
 function setFontSize() {
-    var value = JSON.parse(window.localStorage.getItem('fontSizeIndex'));
-    window.fn.changeFontSize(value);
+    var value = JSON.parse(window.localStorage.getItem("fontSizeIndex"));
+    if (value)
+        window.fn.changeFontSize(value);
 }
 
-window.fn.requestArticle = function (){
-	window.data.articles = getNewArticles();
-}
+window.fn.requestArticle = function() {
+    window.data.articles = getNewArticles();
+};
 
-window.fn.getSources = function(){
+window.fn.getSources = function() {
     window.data.sources = getSources();
-}
+};
 
-window.fn.openArticle = function(i){
-	var articleOptions = {
-		Title: window.data.articles[i].Title,
-		Img: window.data.articles[i].PictureSrc,
-		Content: window.data.articles[i].Content,
-		Date: window.data.articles[i].Date,
-		Source: window.data.articles[i].SourceWebsite
-	}
-	articlesNavi.pushPage('articleView.html', { data : articleOptions } );
-}
+window.fn.openArticle = function(i) {
+    var articleOptions = {
+        Title: window.data.articles[i].Title,
+        Img: window.data.articles[i].PictureSrc,
+        Content: window.data.articles[i].Content,
+        Date: window.data.articles[i].Date,
+        Source: window.data.articles[i].SourceWebsite
+    };
+    articlesNavi.pushPage("articleView.html", { data: articleOptions });
+};
 
-function getWhatArticlesToShow(){
+function getWhatArticlesToShow() {
     //finds index of selected sources in sources list
-    var checkedSources = JSON.parse(localStorage.getItem('checkedSources'));
+    var checkedSources = JSON.parse(localStorage.getItem("checkedSources"));
     var sourcesIndexes = [];
-    for (var j =0; j < checkedSources.length; j++){
-        if (checkedSources[j]){
-            sourcesIndexes.push(j);
+    if (checkedSources && checkedSources.length > 0) {
+        for (var j = 0; j < checkedSources.length; j++) {
+            if (checkedSources[j]) {
+                sourcesIndexes.push(j);
+            }
+
         }
     }
     //finds which articles to show according to selected sources
     window.data.articlesToShow = [];
-    for (var i=0; i< window.data.articles.length; i++){
-        for (var j=0; j<sourcesIndexes.length; j++){
-            if (window.data.articles[i].SourceWebsite == window.data.sources[sourcesIndexes[j]].URL){
+    for (var i = 0; i < window.data.articles.length; i++) {
+        for (var j = 0; j < sourcesIndexes.length; j++) {
+            if (
+                window.data.articles[i].SourceWebsite ==
+                window.data.sources[sourcesIndexes[j]].URL
+            ) {
                 window.data.articlesToShow.push(window.data.articles[i]);
             }
         }
     }
 }
 
-var lastUpdatedDate= null;
+var lastUpdatedDate = null;
 
-function getSources(){
+function getSources() {
     var arrayOfSources = [];
     var i;
-    for (i= 0; i<templateSources.length; i++) {
+    for (i = 0; i < templateSources.length; i++) {
         arrayOfSources.push(templateSources[i]);
     }
     return arrayOfSources;
 }
 
-var countRetrievedArticles= 0;
-function getNewArticles (websiteSources) {
+var countRetrievedArticles = 0;
+
+function getNewArticles(websiteSources) {
     if (lastUpdatedDate == null) {
-        lastUpdatedDate= new Date ();
+        lastUpdatedDate = new Date();
     }
 
     // HTTP Request to the BackEnd API
@@ -176,17 +222,17 @@ function getNewArticles (websiteSources) {
     // For the demostration:
     var arrayOfArticles = [];
     var i;
-    for (i= countRetrievedArticles; i< (countRetrievedArticles+10); i++) {
-        arrayOfArticles.push(templateArticles[i%templateArticles.length]);
+    for (i = countRetrievedArticles; i < countRetrievedArticles + 10; i++) {
+        arrayOfArticles.push(templateArticles[i % templateArticles.length]);
     }
-    countRetrievedArticles= countRetrievedArticles + 10;
-	lastUpdatedDate= new Date ();
+    countRetrievedArticles = countRetrievedArticles + 10;
+    lastUpdatedDate = new Date();
 
     return arrayOfArticles;
 }
 
-function getArticle(index){
-	return window.fn.articles[i];
+function getArticle(index) {
+    return window.fn.articles[i];
 }
 
 init();
--- Mobile_App/js/night_mode_handler.js
+++ Mobile_App/js/night_mode_handler.js
@@ -1,82 +1,86 @@
 /*method responsible for toggling the night mode modifier on and off the elements*/
-window.fn.nightMode = function (el) {
-    var value = el.checked; document.documentElement.classList.add('night');
+window.fn.nightMode = function(el) {
+    var value = el.checked;
+    document.documentElement.classList.add('night');
     window.localStorage.setItem('isNightMode', value);
     setNightMode();
 };
 
 
 /*enables/disables night mode*/
-function setNightMode() {
+function setNightMode(page) {
+    if (page == undefined || page.matches("#settings")) {
+        var thermometerIcons = document.getElementsByTagName('ons-icon');
+        var range = document.getElementById('ToxicToleranceSlider');
+        var markupToxicDiv = document.getElementById('markupToxicDiv');
+        var hideToxicDiv = document.getElementById('hideToxicDiv');
+    }
     var isNightMode = JSON.parse(window.localStorage.getItem('isNightMode'));
     var pages = document.getElementsByTagName('ons-page');
     var lists = document.getElementsByTagName('ons-list');
     var sidemenuList = document.getElementById('sidemenuList');
-	var thermometerIcons = document.getElementsByTagName('ons-icon');
-	var fontChangeTag = document.getElementsByTagName('fontChangeTag');
-	var range = document.getElementById('ToxicToleranceSlider');
-	var markupToxicDiv = document.getElementById('markupToxicDiv');
-	var hideToxicDiv = document.getElementById('hideToxicDiv');
+    var fontChangeTag = document.getElementsByTagName('fontChangeTag');
 
-	if (markupToxicDiv)
-		if (isNightMode)
-			markupToxicDiv.classList.replace('mainStyle_normal_mode', 'mainStyle_night_mode');
-		else
-			markupToxicDiv.classList.replace('mainStyle_night_mode', 'mainStyle_normal_mode');
-			
-	if (hideToxicDiv)
-		if (isNightMode)
-			hideToxicDiv.classList.replace('mainStyle_normal_mode', 'mainStyle_night_mode');
-		else
-			hideToxicDiv.classList.replace('mainStyle_night_mode', 'mainStyle_normal_mode');
+    if (markupToxicDiv)
+        if (isNightMode)
+            markupToxicDiv.classList.replace('mainStyle_normal_mode', 'mainStyle_night_mode');
+        else
+            markupToxicDiv.classList.replace('mainStyle_night_mode', 'mainStyle_normal_mode');
 
-	if (thermometerIcons){
-		for (var i=0; i<thermometerIcons.length; i++){
-			if (isNightMode && thermometerIcons[i].id == 'thermometerIcon'){
-				thermometerIcons[i].style.color = 'white';
-			}else if (!isNightMode && thermometerIcons[i].id == 'thermometerIcon'){
-				thermometerIcons[i].style.color = 'black';
-			}
-		}
-	}
+    if (hideToxicDiv)
+        if (isNightMode)
+            hideToxicDiv.classList.replace('mainStyle_normal_mode', 'mainStyle_night_mode');
+        else
+            hideToxicDiv.classList.replace('mainStyle_night_mode', 'mainStyle_normal_mode');
 
-	if (fontChangeTag){
-		for (var i=0; i< fontChangeTag.length; i++){
-			if (isNightMode && (fontChangeTag[i].id !='noChangeColor' && fontChangeTag[i].id !='articleSource' && fontChangeTag[i].id !='articleDate' && fontChangeTag[i].id != 'fontSizeText')){
-				fontChangeTag[i].style.color = 'white';
-			}else if (!isNightMode && (fontChangeTag[i].id != 'noChangeColor' && fontChangeTag[i].id !='articleSource' && fontChangeTag[i].id !='articleDate' && fontChangeTag[i].id != 'fontSizeText') ){
-				fontChangeTag[i].style.color = 'black';
-			}else{
-				continue;
-			}
-		}
-	}
-	if (pages){
-		for (var i = 0; i < pages.length; i++) {
-			if (isNightMode) {
-				ons.modifier.add(pages[i], 'night_mode_bg');
-				ons.modifier.remove(pages[i], 'normal_mode_bg');
-			} else {
-				ons.modifier.add(pages[i], 'normal_mode_bg');
-				ons.modifier.remove(pages[i], 'night_mode_bg');
-			}
-		}
+    if (thermometerIcons) {
+        for (var i = 0; i < thermometerIcons.length; i++) {
+            if (isNightMode && thermometerIcons[i].id == 'thermometerIcon') {
+                thermometerIcons[i].style.color = 'white';
+            } else if (!isNightMode && thermometerIcons[i].id == 'thermometerIcon') {
+                thermometerIcons[i].style.color = 'black';
+            }
+        }
     }
-	if (lists){
-		for (var i = 0; i < lists.length; i++) {
-			if (isNightMode) {
-				ons.modifier.add(lists[i], 'night_mode_bg');
-				ons.modifier.remove(lists[i], 'normal_mode_bg');
-			} else {
-				ons.modifier.add(lists[i], 'normal_mode_bg');
-				ons.modifier.remove(lists[i], 'night_mode_bg');
-			}
-		}
-	}
+
+    if (fontChangeTag) {
+        for (var i = 0; i < fontChangeTag.length; i++) {
+            if (isNightMode && (fontChangeTag[i].id != 'noChangeColor' && fontChangeTag[i].id != 'articleSource' && fontChangeTag[i].id != 'articleDate' && fontChangeTag[i].id != 'fontSizeText')) {
+                fontChangeTag[i].style.color = 'white';
+            } else if (!isNightMode && (fontChangeTag[i].id != 'noChangeColor' && fontChangeTag[i].id != 'articleSource' && fontChangeTag[i].id != 'articleDate' && fontChangeTag[i].id != 'fontSizeText')) {
+                fontChangeTag[i].style.color = 'black';
+            } else {
+                continue;
+            }
+        }
+    }
+    if (pages) {
+        for (var i = 0; i < pages.length; i++) {
+            if (isNightMode) {
+                ons.modifier.add(pages[i], 'night_mode_bg');
+                ons.modifier.remove(pages[i], 'normal_mode_bg');
+            } else {
+                ons.modifier.add(pages[i], 'normal_mode_bg');
+                ons.modifier.remove(pages[i], 'night_mode_bg');
+            }
+        }
+    }
+    if (lists) {
+        for (var i = 0; i < lists.length; i++) {
+            if (isNightMode) {
+                ons.modifier.add(lists[i], 'night_mode_bg');
+                ons.modifier.remove(lists[i], 'normal_mode_bg');
+            } else {
+                ons.modifier.add(lists[i], 'normal_mode_bg');
+                ons.modifier.remove(lists[i], 'night_mode_bg');
+            }
+        }
+    }
     if (sidemenuList)
         isNightMode ? ons.modifier.add(sidemenuList, 'night_mode_bg') : ons.modifier.remove(sidemenuList, 'night_mode_bg');
 };
 
+//sets the sources list to night mode
 function setSourcesNightMode() {
     var value = JSON.parse(window.localStorage.getItem('isNightMode'));
     var sourcesList = document.getElementById('sourcesList');
